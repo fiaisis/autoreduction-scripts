384c0e1af994f2c55f082e1769bb20b774ebd49b
Keiran Price <keiran.price@stfc.ac.uk>
2023-04-27 08:46:52 +0100

    Update the test script

--- TEST/reduce.py
+++ TEST/reduce.py
@@ -1,1 +1,14 @@
-raise Exception('script not implemented')
+from __future__ import print_function
+
+print("Doing some science")
+
+x = 4
+y = 2
+
+for i in range(20):
+    x *= y
+
+def something() -> None:
+    return
+
+something()
