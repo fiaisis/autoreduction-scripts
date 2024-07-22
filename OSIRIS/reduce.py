ab9f8045339e3eb8a1233c8cd831b55b658d5ab1
Samuel Jones <samjones714@gmail.com>
2024-07-22 16:05:34 +0100

    Update reduce.py

--- OSIRIS/reduce.py
+++ OSIRIS/reduce.py
@@ -5,37 +5,9 @@ import requests as requests
 def generate_input_path_for_run(run_number, cycle):
     return f"/archive/ndxosiris/Instrument/data/{cycle}/OSI{run_number}.nxs"
 
-def get_file_from_request(url: str, path: str) -> None:
-    """
-    write the file from the url to the given path, retrying at most 3 times
-    :param url: the url to get
-    :param path: the path to write to
-    :return: None
-    """
-    success = False
-    attempts = 0
-    wait_time_seconds = 15
-    while attempts < 3:
-        print(f"Attempting to get resource {url}", flush=True)
-        response = requests.get(url)
-        if not response.ok:
-            print(f"Failed to get resource from: {url}", flush=True)
-            print(f"Waiting {wait_time_seconds}...", flush=True)
-            time.sleep(wait_time_seconds)
-            attempts += 1
-            wait_time_seconds *= 3
-        else:
-            with open(path, "w+") as fle:
-                fle.write(response.text)
-            success = True
-            break
-
-    if not success:
-        raise RuntimeError(f"Reduction not possible with missing resource {url}")
 
 # To change by automatic script
 input_runs = [108538, 108539]
-# This needs to be loaded from a shared repository of files
 calibration_run_number = "00148587"
 cycle = "cycle_14_1"
 analyser = "graphite"
@@ -46,7 +18,7 @@ diffraction_reduction = False
 if not diffraction_reduction and not spectroscopy_reduction:
     raise RuntimeError("diffraction_reduction and spectroscopy_reduction are both false, so this will do nothing.")
 
-# Defaults
+# Defaults and other generated inputs
 instrument = "OSIRIS"
 instrument_definition_directory = ConfigService.Instance().getString("instrumentDefinition.directory")
 instrument_filename = instrument_definition_directory + instrument + "_Definition.xml"
@@ -58,10 +30,8 @@ spec_spectra_range = "963,1004"
 diff_spectra_range = '3,962'
 unit_x = "DeltaE"
 fold_multiple_frames = False
-diffraction_calibration_file = "osiris_041_RES10.cal"
-get_file_from_request("https://raw.githubusercontent.com/fiaisis/autoreduction-scripts/cbf4e37365e112334bc7cee601553ebd0dbacc1d/OSIRIS/osiris_041_RES10.cal", diffraction_calibration_file)
+diffraction_calibration_file = "/extras/osiris_041_RES10.cal"
 
-# Generated
 sum_runs = len(input_runs) > 1
 
 input_file_paths = ""
@@ -71,15 +41,9 @@ input_file_paths = input_file_paths[2:]  # Slice out the excess ", "
 print(input_file_paths)
 output_workspaces = []
 
-# Generate calibration workspace
 
+# Generate calibration workspace function
 def generate_spec_calibration_workspace():
-    # The following is already done above to generate the eFixed number:
-    # instrument_definition_directory = ConfigService.Instance().getString("instrumentDefinition.directory")
-    # instrument_filename = instrument_definition_directory + instrument + "_Definition.xml"
-    # instrument_workspace = LoadEmptyInstrument(Filename=instrument_filename, OutputWorkspace="instrument_workspace")
-    # parameter_filename = instrument_definition_directory + instrument + "_" + analyser + "_" + reflection + "_Parameters.xml"
-    # parameter_file = LoadParameterFile(Filename=parameter_filename, Workspace="instrument_workspace")
     resolution = instrument_workspace.getInstrument().getComponentByName(analyser).getNumberParameter("resolution", True)[0]
     x = [-6 * resolution, -5 * resolution, -2 * resolution, 0, 2 * resolution]
     y = [1, 2, 3, 4]
@@ -122,11 +86,10 @@ if spectroscopy_reduction:
     output_workspaces.append(output_diffspec_ws)
 # 
 # elif diffraction_reduction:
-#     calibration_workspace = generate_spec_calibration_workspace()
 #     print("Producing a diffraction reduction")
 #     for run_number in input_runs:
 #         ws_name = instrument + run_number
-#         drange_ws = OSIRISDiffractionReduction(Sample=ws_name, CalFile=calibration_file_path, OutputWorkspace=f'{ws_name}_dRange')
+#         drange_ws = OSIRISDiffractionReduction(Sample=ws_name, CalFile=diffraction_calibration_file, OutputWorkspace=f'{ws_name}_dRange')
 #         q_ws = ConvertUnits(InputWorkspace=drange_ws, OutputWorkspace=f'{ws_name}_q', Target='QSquared')
 #         tof_ws = ConvertUnits(InputWorkspace=drange_ws, OutputWorkspace=f'{ws_name}_tof', Target='TOF')
 #         output_workspaces.append(drange_ws)
