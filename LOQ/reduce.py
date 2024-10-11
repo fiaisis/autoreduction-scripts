a9e9e6b97d6c98ddf5376a017ba8a9231589680a
Samuel Jones <samjones714@gmail.com>
2024-10-11 16:34:12 +0100

    Update reduce.py

--- LOQ/reduce.py
+++ LOQ/reduce.py
@@ -1,8 +1,7 @@
 from mantid.kernel import ConfigService
 import math
-
 import numpy
-from mantid.simpleapi import RenameWorkspace, SaveRKH, SaveNXcanSAS, GroupWorkspaces, SaveNexusProcessed
+from mantid.simpleapi import RenameWorkspace, SaveRKH, SaveNXcanSAS, GroupWorkspaces, mtd
 from mantid import config
 from sans.user_file.toml_parsers.toml_reader import TomlReader
 import sans.command_interface.ISISCommandInterface as ici
@@ -15,56 +14,67 @@ sample_direct = None
 can_scatter = None
 can_transmission = None
 can_direct = None
+sample_thickness = 1.0
+sample_geometry = "Disc"
+sample_height = 8.0
+sample_width = 8.0
 
-# Save and reduction options
+# Other configuration options
 output_path = f"/output/run-{sample_scatter}/"
 config['defaultsave.directory'] = output_path
 ConfigService.setDataSearchDirs("/archive/NDXLOQ/user/masks/")
-
-
-def grab_wavelength_limits_from_user_file():
-    # Will result in an extra parsing of the file but needed here.
-    read_toml = TomlReader.get_user_file_dict(user_file)
-    wavelength_details = read_toml['binning']['wavelength']
-    step = wavelength_details['step']
-    start = wavelength_details['start']
-    stop = wavelength_details['stop']
-    if wavelength_details['type'] == "Log":
-        log_step = 1 + step
-        wavs = [start]
-        while True:
-            next_value = wavs[-1] * log_step
-            if next_value > stop:
-                break
-            else:
-                wavs.append(next_value)
-        wavs[-1] = stop
-        return wavs
-    elif wavelength_details['type'] == "Lin":
-        return list(numpy.arange(start=start, stop=stop, step=step))
-    else:
-        raise ValueError(f"User file does not contain Log or Lin ranges for wavelengths uses type "
-                         f"{wavelength_details['type']}")
-
-
-wavs = grab_wavelength_limits_from_user_file()
+default_slice_wavs = [2.7, 3.7, 4.7, 5.7, 6.7, 8.7, 10.5]
 
 output = []
 
 # Setup ISIS Command Interface for 1D
 def cleanup_and_setup_ici():
-    ici.Clean()
-    ici.UseCompatibilityMode()
-    ici.LOQ()
-    ici.Set1D()
-    ici.MaskFile(user_file)
-    ici.AssignSample(str(sample_scatter))
-    if sample_transmission is not None and sample_direct is not None:
-        ici.TransmissionSample(str(sample_transmission), str(sample_direct))
-    if can_scatter is not None:
-        ici.AssignCan(str(can_scatter))
-    if can_scatter is not None and can_transmission is not None and can_direct is not None:
-        ici.TransmissionCan(str(can_transmission), str(can_direct))
+  ici.Clean()
+  ici.UseCompatibilityMode()
+  ici.LOQ()
+  ici.Set1D()
+  ici.MaskFile(user_file)
+  ici.AssignSample(str(sample_scatter))
+  if sample_transmission is not None and sample_direct is not None:
+      ici.TransmissionSample(str(sample_transmission), str(sample_direct))
+  if can_scatter is not None:
+      ici.AssignCan(str(can_scatter))
+  if can_scatter is not None and can_transmission is not None and can_direct is not None:
+      ici.TransmissionCan(str(can_transmission), str(can_direct))
+
+      
+def get_nxcansas_kwargs(workspace_name: str, sector: bool = False, ws_suffix: str | None = None):
+  if ws_suffix is None:
+    # Get the last 2 numbers from workspace and add them to suffix
+    if not sector:
+      workspace_name_split = workspace_name.split("_")
+    else:
+      workspace_name_split = workspace_name.split("Phi")[0].split("_")
+    ws_suffix = f"{workspace_name_split[-2]}_{workspace_name_split[-1]}"
+  kwargs = {}
+  kwargs["InputWorkspace"] = workspace_name
+  kwargs["Filename"] = f"{output_path}/{workspace_name}_auto.h5"
+  kwargs["Geometry"] = sample_geometry
+  kwargs["SampleHeight"] = sample_height
+  kwargs["SampleWidth"] = sample_width
+  kwargs["SampleThickness"] = sample_thickness
+  if "merged" in workspace_name.lower():
+    kwargs["DetectorNames"] = "main-detector-bank,HAB"
+  elif "hab" in workspace_name.lower():
+    kwargs["DetectorNames"] = "HAB"
+  elif "lab" in workspace_name.lower():
+    kwargs["DetectorNames"] = "main-detector-bank"
+  if sample_transmission is not None and sample_direct is not None:
+    kwargs["Transmission"] = f"{sample_scatter}_trans_Sample_{ws_suffix}"
+    kwargs["SampleTransmissionRunNumber"] = str(sample_transmission)
+    kwargs["SampleDirectRunNumber"] = str(sample_direct)
+  if can_scatter is not None:
+    kwargs["CanScatterRunNumber"] = str(can_scatter)
+  if can_scatter is not None and can_transmission is not None and can_direct is not None:
+    kwargs["TransmissionCan"] = f"{sample_scatter}_trans_Can_{ws_suffix}"
+    kwargs["CanDirectRunNumber"] = str(can_direct)
+  return kwargs
+
 
 # Perform 1D reduction and output
 cleanup_and_setup_ici()
@@ -72,10 +82,10 @@ output_workspace_1d_merged = ici.WavRangeReduction(None, None, ici.DefaultTrans,
 output_workspace_1d_hab = output_workspace_1d_merged.replace("merged", "HAB")
 output_workspace_1d_lab = output_workspace_1d_merged.replace("merged", "main")
 for output_workspace in [output_workspace_1d_merged, output_workspace_1d_hab, output_workspace_1d_lab]:
-    SaveNXcanSAS(output_workspace, f"{output_path}/{output_workspace}.h5")
-    output.append(f"{output_workspace}.h5")
-    SaveRKH(output_workspace, f"{output_path}/{output_workspace}.dat")
-    output.append(f"{output_workspace}.dat")
+  SaveNXcanSAS(**get_nxcansas_kwargs(output_workspace))
+  output.append(f"{output_workspace}_auto.h5")
+  SaveRKH(output_workspace, f"{output_path}/{output_workspace}_auto.dat")
+  output.append(f"{output_workspace}_auto.dat")
 
 # Setup ISIS Command Interface for 2D and reduce
 ici.Set2D()
@@ -83,31 +93,20 @@ output_workspace_2d_merged = ici.WavRangeReduction(None, None, ici.DefaultTrans,
 output_workspace_2d_hab = output_workspace_2d_merged.replace("merged", "HAB")
 output_workspace_2d_lab = output_workspace_2d_merged.replace("merged", "main")
 for output_workspace in [output_workspace_2d_merged, output_workspace_2d_hab, output_workspace_2d_lab]:
-    SaveNXcanSAS(output_workspace, f"{output_path}/{output_workspace}.h5")
-    output.append(f"{output_workspace}.h5")
-
-# Now perform the overlap reduction
-ici.Set1D()
-output_workspaces_overlap_reductions_name_string = ""
-for i in range(len(wavs) - 1):
-    output_workspaces_overlap_reductions_name_string += f", {ici.WavRangeReduction(wavs[i], wavs[i + 1], False, combineDet='merged')}"
-overlap_reduction_name = f"{scatter}_overlap_reduction_group_workspace"
-GroupWorkspaces(InputWorkspaces=output_workspaces_overlap_reductions_name_string, OutputWorkspace=overlap_reduction_name)
-SaveNexusProcessed(overlap_reduction_name, f"{output_path}/{overlap_reduction_name}.nxs")
-output.append(f"{overlap_reduction_name}.nxs")
-
+  SaveNXcanSAS(**get_nxcansas_kwargs(output_workspace))
+  output.append(f"{output_workspace}_auto.h5")
 
 def save_sector_reduction(output_workspace, sector):
-    # Assume merged
-    def save_out_ws(output_workspace_inside, sector):
-        RenameWorkspace(output_workspace_inside, output_workspace_inside + sector)
-        SaveNXcanSAS(output_workspace_inside + sector, f"{output_path}/{output_workspace_inside}{sector}.h5")
-        output.append(output_workspace_inside + sector)
-    output_workspace_hab = output_workspace.replace("merged", "HAB")
-    output_workspace_lab = output_workspace.replace("merged", "main")
-    save_out_ws(output_workspace, sector)
-    save_out_ws(output_workspace_lab, sector)
-    save_out_ws(output_workspace_hab, sector)
+  # Assume merged
+  def save_out_ws(output_workspace_inside, sector):
+      RenameWorkspace(output_workspace_inside, output_workspace_inside + sector)
+      SaveNXcanSAS(**get_nxcansas_kwargs(output_workspace_inside + sector, sector=True))
+      output.append(output_workspace_inside + sector + "_auto")
+  output_workspace_hab = output_workspace.replace("merged", "HAB")
+  output_workspace_lab = output_workspace.replace("merged", "main")
+  save_out_ws(output_workspace, sector)
+  save_out_ws(output_workspace_lab, sector)
+  save_out_ws(output_workspace_hab, sector)
 
 
 # Now perform the sector reduction
@@ -119,3 +118,11 @@ save_sector_reduction(output_workspaces, "_horizontal_sector")
 ici.SetPhiLimit(60, 120, use_mirror=True)
 output_workspaces=ici.WavRangeReduction(None, None, ici.DefaultTrans, combineDet="merged")
 save_sector_reduction(output_workspaces, "_vertical_sector")
+
+# Now perform the overlap reduction
+cleanup_and_setup_ici()
+ici.Set1D()
+for i in range(len(default_slice_wavs) - 1):
+  output_workspace = ici.WavRangeReduction(default_slice_wavs[i], default_slice_wavs[i + 1], False, combineDet='merged')
+  SaveNXcanSAS(**get_nxcansas_kwargs(output_workspace, ws_suffix=f"{default_slice_wavs[0]}_{default_slice_wavs[-1]}"))
+  output.append(f"{output_workspace}_auto.h5")
