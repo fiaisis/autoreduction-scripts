46f43adf00807dc203218bfaf4df25ff4bae601c
Samuel Jones <samjones714@gmail.com>
2025-02-18 14:03:09 +0000

    Update reduce.py

--- MARI/reduce.py
+++ MARI/reduce.py
@@ -34,11 +34,12 @@ def get_file_from_request(url: str, path: str) -> None:
     if not success:
         raise RuntimeError(f"Reduction not possible with missing resource {url}")
 
+git_sha = "5a0b0a76caad4252465e9f889fbe18f82dd41d47"
 # Only needed for fixes with regards to reductions during MARI issues 
-get_file_from_request("https://raw.githubusercontent.com/mantidproject/direct_reduction/a3e1531c4fdffe006c4b13dc61a69c71cea8fda5/reduction_files/reduction_utils.py", "reduction_utils.py")
-get_file_from_request("https://raw.githubusercontent.com/mantidproject/direct_reduction/a3e1531c4fdffe006c4b13dc61a69c71cea8fda5/reduction_files/DG_whitevan.py", "DG_whitevan.py")
-get_file_from_request("https://raw.githubusercontent.com/mantidproject/direct_reduction/a3e1531c4fdffe006c4b13dc61a69c71cea8fda5/reduction_files/DG_reduction.py", "DG_reduction.py")
-get_file_from_request("https://raw.githubusercontent.com/mantidproject/direct_reduction/a3e1531c4fdffe006c4b13dc61a69c71cea8fda5/reduction_files/DG_monovan.py", "DG_monovan.py")
+get_file_from_request(f"https://raw.githubusercontent.com/mantidproject/direct_reduction/{git_sha}/reduction_files/reduction_utils.py", "reduction_utils.py")
+get_file_from_request(f"https://raw.githubusercontent.com/mantidproject/direct_reduction/{git_sha}/reduction_files/DG_whitevan.py", "DG_whitevan.py")
+get_file_from_request(f"https://raw.githubusercontent.com/mantidproject/direct_reduction/{git_sha}/reduction_files/DG_reduction.py", "DG_reduction.py")
+get_file_from_request(f"https://raw.githubusercontent.com/mantidproject/direct_reduction/{git_sha}/reduction_files/DG_monovan.py", "DG_monovan.py")
 
 # Temporarily not needed:
 # get_file_from_request("https://raw.githubusercontent.com/mantidproject/scriptrepository/master/direct_inelastic/"
