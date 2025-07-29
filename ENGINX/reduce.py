741ca99f8a7ad2e96f598002ebcfada3c351bb58
Keiran Price <keiran.price@stfc.ac.uk>
2025-07-29 11:07:45 +0100

    Rearange to add clarity

--- ENGINX/reduce.py
+++ ENGINX/reduce.py
@@ -1,31 +1,31 @@
-# import mantid algorithms, numpy and matplotlib
 from pathlib import Path
 
 from mantid.simpleapi import *
 from Engineering.EnginX import EnginX
 from Engineering.EnggUtils import GROUP
 
-FULL_CALIB = "/opt/conda/envs/mantid/scripts/Engineering/calib/ENGINX_full_instrument_calibration_193749.nxs"
 
+# Values changed by rundetection
 vanadium_run = "ENGINX236516"
 focus_runs = ["ENGINX299080"]
 ceria_run = "ENGINX193749"
 group = GROUP["BOTH"]
 
-run_dir = [focus_runs[0][6:]]
-CWDIR = f"/output/run-{run_dir}"
+# Set values that don't change
+output_dir = f"/output/run-{focus_runs[0]}"
+calib_file = "/home/sam/miniforge/envs/mantid/scripts/Engineering/calib/ENGINX_full_instrument_calibration_193749.nxs"
 
 output = []  # This is probably the nexus files in the focus dir or something
 enginx = EnginX(
     vanadium_run=vanadium_run,
     focus_runs=focus_runs,
-    save_dir=CWDIR,
-    full_inst_calib_path=FULL_CALIB,
+    save_dir=output_dir,
+    full_inst_calib_path=calib_file,
     ceria_run=ceria_run,
     group=GROUP.BOTH,
 )
 enginx.main(plot_cal=False, plot_foc=False)
 
-for path in Path(CWDIR).rglob("*"):
+for path in Path(output_dir).rglob("*"):
     if path.is_file():
         output.append(str(path))
