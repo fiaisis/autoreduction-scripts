99bce707fba9d6adf6a4f211e2f632d260b0be01
keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>
2025-11-20 15:23:48 +0000

    Update file name format in reduce.py

--- VESUVIO/reduce.py
+++ VESUVIO/reduce.py
@@ -75,7 +75,7 @@ file_name = (
     requests.get(
         f"http://data.isis.rl.ac.uk/where.py/unixdir?name=VESUVIO{runno}"
     ).text.strip("\n")
-    + f"/VESUVIO000{runno}"
+    + f"/VESUVIO000{runno}.nxs"
 )
 
 print(f"Starting with file: {file_name}")
